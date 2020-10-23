0cafa90094790dad706f0ab5b16e24cb5e67bfe1
Nines <921944921@qq.com>
2020-10-23 19:36:52 +0800

    Update config.js
    更新 gtihub app 授权

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const isProduction = process.env.NODE_ENV === 'production'
-const clientId = isProduction ? '75cf00b02deb33e63424' : 'bf9d4f20def51dbd5c5c'
-const clientSecret = isProduction ? '6fa564cbd46f6bdfa1fb81ddce5503dcbe4ab4c4' : 'e8f5bc4369f64b05d052819047ca9d5e1d8b2e8f'
+const clientId = isProduction ? 'e5c9cd9b2899ae50dc61' : 'e5c9cd9b2899ae50dc61'
+const clientSecret = isProduction ? 'e0ba07e1cc20641e20dd426e267b40f7b28ae6ed' : 'e0ba07e1cc20641e20dd426e267b40f7b28ae6ed'
 
 export default {
   clientId,
